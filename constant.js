47593f85770a77d4f3b91e30c5b7bd05c26f42fa
agate-pris <agate-pris@users.noreply.github.com>
2016-10-25 19:44:14 +0900

    move constant into constant.js

---
+++ constant.js
@@ -0,0 +1,36 @@
+
+// constant
+// =====================================================================
+
+// StreamTypeEnum
+// http://msdn.microsoft.com/ja-jp/library/cc389884.aspx
+// ---------------------------------------------------------------------
+
+var adTypeBinary = 1;
+var adTypeText   = 2;
+
+// StreamReadEnum
+// http://msdn.microsoft.com/ja-jp/library/cc389881.aspx
+// ---------------------------------------------------------------------
+
+var adReadLine = -2;
+
+// StreamWriteEnum
+// http://msdn.microsoft.com/ja-jp/library/cc389886.aspx
+// ---------------------------------------------------------------------
+
+var adWriteChar = 0;
+var adWriteLine = 1;
+
+// SaveOptionsEnum 
+// http://msdn.microsoft.com/ja-jp/library/cc389870.aspx
+// ---------------------------------------------------------------------
+
+var adSaveCreateOverWrite = 2;
+
+// ConnectModeEnum
+// https://msdn.microsoft.com/ja-jp/library/cc389773.aspx
+// ---------------------------------------------------------------------
+
+var adModeWrite     = 2;
+var adModeReadWrite = 3;
